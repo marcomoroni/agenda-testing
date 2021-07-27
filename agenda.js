3a0ac29f09aa26cdf09afd26bb7a439fdb5a5d30
Marco Moroni <marcomoroni394@gmail.com>
2021-07-27 22:02:20 +0100

    -

---
+++ agenda.js
@@ -0,0 +1,27 @@
+"use strict"
+
+window.addEventListener('load', (event) => {
+    // Init app.
+    // TODO: Add to an 'app' file.
+    document.body.innerHTML += `
+        <div class="app-container">
+            <div class="fake-rounded-corners">
+            </div>
+        </div>
+    `
+    var appRootEl = document.getElementsByClassName(`fake-rounded-corners`)[0]
+
+    // ...
+})
+
+
+// UI.
+// TODO: Add to a UI library file.
+
+function column(children) {
+
+}
+
+function scaffoldOrModal(child) {
+
+}
