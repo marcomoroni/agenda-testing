fa80cbc8e9a3abe5998bd9acad8dced22b641215
Marco Moroni <marcomoroni394@gmail.com>
2021-08-01 22:38:05 +0100

    -

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "af257398055230a145eb8059593dfcc2",
-"/": "af257398055230a145eb8059593dfcc2",
-"main.dart.js": "3d57550533f5ae1c12dcba644877e394",
+"index.html": "076b165e6b1d382fc221c6f2a308bb9f",
+"/": "076b165e6b1d382fc221c6f2a308bb9f",
+"main.dart.js": "7186a5d61a4a34d04dabfce3379e130f",
 "manifest.json": "8c6c26d61a213a121f81a2970a59fa56",
 "version.json": "8515b0c4a6b864bd5bc0af9e2df2931d"
 };
